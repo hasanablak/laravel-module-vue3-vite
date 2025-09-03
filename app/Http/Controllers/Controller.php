04d57e85d487926963353c20893d0b136dd44964
Hasan Ablak <0hasanablak@gmail.com>
2025-09-03 23:21:26 +0300

    added general response methods

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,25 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+	public function success($data = [], $message = "Successfully fetched")
+	{
+		return response()->json([
+			"status" => "success",
+			"data" => $data,
+			"message" => $message
+		]);
+	}
+
+	public function error($data = [], $message = "Something went wrong", $statusCode = 500)
+	{
+		return response()
+			->json(
+				data: [
+					"status" => "error",
+					"message" => $message,
+					"data" => $data
+				],
+				status: $statusCode
+			);
+	}
 }
